32e64b36844134d95603ef76e5b469198dda7cef
Priyanka sahani <priyanka.sahani.sp@gmail.com>
2026-09-10 17:18:42 +0530

    Update allowed hosts for Render

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,11 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [ 
+    "localhost",
+    "127.0.0.1",
+    "notification-system-1-cumz.onrender.com",
+    ]
 
 
 # Application definition
